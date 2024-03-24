34b2df72873658fefc96802e38d31be324371a2e
Nivedha <nivedha.vijayakumar@multicorewareinc.com>
2024-03-24 09:21:56 +0530

    added tools page and rendered predicted expense of current month

--- app.py
+++ app.py
@@ -47,8 +47,6 @@ def add_entry():
 @app.route('/predict_expense')
 def predict_expense():
     # Implement prediction logic based on historical data
-    # This could involve training a machine learning model on past entries
-    # For simplicity, we'll assume a basic rule-based prediction for now
     transaction = list(collection.find()) 
     transaction_df = pd.DataFrame(transaction)
     transaction_df['amount']=transaction_df['amount'].astype('float')
@@ -72,8 +70,8 @@ def predict_expense():
     income_df = income_df.dropna(subset=['date'])
     date=expense_df['date'].dt.month
     expense_df.set_index('date').groupby(pd.Grouper(freq='M'))
-    monthly_spending = expense_df.set_index('date').groupby(pd.Grouper(freq='M')).amount
-    monthly_income = income_df.set_index('date').groupby(pd.Grouper(freq='M')).amount
+    monthly_spending = expense_df.set_index('date').groupby(pd.Grouper(freq='M')).amount.sum()
+    monthly_income = income_df.set_index('date').groupby(pd.Grouper(freq='M')).amount.sum()
     
     # Prophet model
     category_code={'Bank':0, 'Clothing':1, 'Debt':2, 'Donations':3, 'Education':4,
@@ -89,10 +87,15 @@ def predict_expense():
     model= p.fit(expense_df_TSA)
     future = p.make_future_dataframe(periods=36,freq='M')
     forecast_prediction = p.predict(future)
+
     next_month_prediction = forecast_prediction[forecast_prediction['ds'] == forecast_prediction['ds'].max()]['yhat'].values[0]
     next_month_prediction=next_month_prediction.round().astype(int)
-    predicted_expense = next_month_prediction
-    return render_template('predict_expense.html', predicted_expense=predicted_expense,avg_spending_category=avg_spending_category) 
+
+    current_month = datetime.now().month
+    current_month_prediction = forecast_prediction[forecast_prediction['ds'].dt.month == current_month]['yhat'].values[0]
+    current_month_prediction=current_month_prediction.round().astype(int)
+
+    return render_template('predict_expense.html', predicted_expense=next_month_prediction,avg_spending_category=avg_spending_category,monthly_spending=monthly_spending, monthly_income=monthly_income, current_month_prediction=current_month_prediction) 
 
 @app.route('/transactions_this_month')
 def transactions_this_month():
@@ -130,16 +133,55 @@ def transactions_this_month():
     income_amount_month=income_df_month.amount.sum()
     total_balance_month=income_amount_month-expense_amount_month 
 
-    predicted_month_expense = 80
+    #predict current month expense
+    transaction = list(collection.find()) 
+    transaction_df = pd.DataFrame(transaction)
+    transaction_df['amount']=transaction_df['amount'].astype('float')
+    income_df=transaction_df[transaction_df['type']== 'Income']
+    income_df['amount']=income_df['amount'].astype('float')
+    expense_df=transaction_df[transaction_df['type']== 'Expense']
+    expense_df['amount']=expense_df['amount'].astype('float')
+    expense_df['date'] = pd.to_datetime(expense_df['date'], errors='coerce')
+    income_df['date'] = pd.to_datetime(income_df['date'], errors='coerce')
+    expense_df = expense_df.dropna(subset=['date'])
+    income_df = income_df.dropna(subset=['date'])
+    expense_df.set_index('date').groupby(pd.Grouper(freq='M'))
+    
+    # Prophet model
+    category_code={'Bank':0, 'Clothing':1, 'Debt':2, 'Donations':3, 'Education':4,
+       'Entertainment':5, 'Fee':6, 'Food':7, 'Gift':8, 'Groceries':9, 'Healthcare':10, 'Home':11, 
+       'Insurance':12, 'Payroll':13, 'Personal':14, 'Savings':15, 'Subscription':16,
+       'Tax':17, 'Transportation':18, 'Utilities':19}
+    expense_df['category_code']=expense_df.category.map(category_code)
+    
+    expense_df_TSA=expense_df
+    expense_df_TSA=expense_df_TSA.drop(['category','type','category_code'],axis=1)
+    expense_df_TSA.rename(columns={'date':'ds','amount':'y'},inplace=True)
+    p=Prophet(interval_width=0.92,daily_seasonality=True)
+    model= p.fit(expense_df_TSA)
+    future = p.make_future_dataframe(periods=36,freq='M')
+    forecast_prediction = p.predict(future)
+
+    current_month = datetime.now().month
+    current_month_prediction = forecast_prediction[forecast_prediction['ds'].dt.month == current_month]['yhat'].values[0]
+    current_month_prediction=current_month_prediction.round().astype(int)
 
     return render_template('transactions_month.html',transactions_month=transactions_month, 
     transactions=transactions,expense_amount=expense_amount,income_amount=income_amount,
     total_balance=total_balance,total_balance_month=total_balance_month,income_amount_month=income_amount_month,
-    expense_amount_month=expense_amount_month, predicted_month_expense=predicted_month_expense)
+    expense_amount_month=expense_amount_month, predicted_month_expense=current_month_prediction)
+
+@app.route('/tools')
+def tools():
+    return render_template('tools.html')
 
 @app.route('/calculator')
 def calc():
     return render_template('calculator.html')
+
+@app.route('/converter')
+def conv():
+    return render_template('converter.html')
 
 @app.route('/statistics')
 def statistics():
